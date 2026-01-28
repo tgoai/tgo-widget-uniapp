4b9ab2e492013c7537e37aced85913026b5c107e
wanglihui <1769794040@qq.com>
2026-01-28 09:49:45 +0800

    feat(widget): 实现产品列表组件及样式优化
    - 新增 ProductListWidget 组件，支持展示产品列表数据
    - 增加标题和副标题显示逻辑，提升组件信息表达
    - 支持商品缩略图、名称及价格的展示
    - 采用网格布局显示产品，提升视觉效果
    - 使用共享样式隔离，确保样式不会被影响或覆盖
    - 使用 SCSS 编写样式，引入 Tailwind CSS 工具类优化样式结构
    - 优化产品项的容器样式包括圆角和背景透明度，实现更美观的展示

--- src/components/widget/ProductListWidget.vue
+++ src/components/widget/ProductListWidget.vue
@@ -1,9 +1,76 @@
 <script setup lang="ts">
 import type { ProductListWidgetData } from './types'
 
+defineOptions({
+  options: {
+    styleIsolation: 'shared',
+  },
+})
+
 const props = defineProps<{ data: ProductListWidgetData }>()
 </script>
 
 <template>
-  <view>ProductListWidgetData</view>
+  <wd-card custom-class="product-list-widget">
+    <view v-if="data.title || data.subtitle" class="product-title">
+      <view class="title-text">
+        {{ data.title }}
+      </view>
+      <view class="subtitle-text">
+        {{ data.subtitle }}
+      </view>
+    </view>
+    <view class="product-list">
+      <view v-for="(product, index) in data.products" :key="index" class="product-item">
+        <view v-if="product.thumbnail" class="product-item-image">
+          <image :src="product.thumbnail" mode="scaleToFill"></image>
+        </view>
+        <view class="product-item-title">
+          {{ product.name }}
+        </view>
+        <view class="product-item-price">
+          ¥{{ product.price }}
+        </view>
+      </view>
+    </view>
+  </wd-card>
 </template>
+
+<style lang="scss">
+.product-list-widget {
+  @apply !mx-0 !bg-#f5f5f5 !py-3;
+
+  .product-title {
+    @apply mb-2;
+    .title-text {
+      @apply flex items-center  text-#333333 text-lg font-bold;
+    }
+    .subtitle-text {
+      @apply text-#666666 text-sm;
+    }
+  }
+
+  .product-list{
+    @apply grid grid-cols-2 gap-4 ;
+
+    .product-item{
+      @apply  bg-gray-700/50 rounded-lg overflow-hidden;
+
+      .product-item-image{
+        @apply w-full h-160rpx;
+        image{
+          @apply w-full h-full;
+        }
+      }
+
+      .product-item-title{
+        @apply text-#fff text-26rpx font-bold px-2 mt-2;
+      }
+
+      .product-item-price{
+        @apply text-#e4393c text-sm font-bold px-2 mb-2;
+      }
+    }
+  }
+}
+</style>
